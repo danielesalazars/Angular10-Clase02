8bb4d84ad28f119ef5101ad7461f7d5221c4a7e1
Daniel Salazar <daniel.esalazars@gmail.com>
2020-10-10 01:03:13 -0500

    Agregando pipe

--- src/app/countries/countries.component.ts
+++ src/app/countries/countries.component.ts
@@ -1,18 +1,25 @@
 import { Component, OnInit } from '@angular/core';
 import * as countries from './../../assets/countries.json';
 import { Country } from '@app/models';
+import { UpperCasePipe } from '@angular/common';
 @Component({
   selector: 'app-countries',
   templateUrl: './countries.component.html',
   styleUrls: ['./countries.component.scss'],
+  providers: [UpperCasePipe],
 })
 export class CountriesComponent implements OnInit {
   selected: Country;
-  constructor() {
+  constructor(private upperCasePipe: UpperCasePipe) {
     //console.log(countries);
     this.selected = countries[10];
     console.log(this.selected);
   }
 
   ngOnInit(): void {}
+
+  send(name: string): void {
+    name = this.upperCasePipe.transform(name);
+    console.log(name);
+  }
 }

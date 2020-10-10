d570d23e4de40c23971ce71c043e11de8df564be
Daniel Salazar <daniel.esalazars@gmail.com>
2020-10-09 20:13:08 -0500

    Agregando ejercicio de mostrar pais en consola

---
+++ src/app/countries/countries.component.ts
@@ -0,0 +1,18 @@
+import { Component, OnInit } from '@angular/core';
+import * as countries from './../../assets/countries.json';
+import { Country } from '../interfaces/country';
+@Component({
+  selector: 'app-countries',
+  templateUrl: './countries.component.html',
+  styleUrls: ['./countries.component.scss'],
+})
+export class CountriesComponent implements OnInit {
+  selected: Country;
+  constructor() {
+    //console.log(countries);
+    this.selected = countries[10];
+    console.log(this.selected);
+  }
+
+  ngOnInit(): void {}
+}
